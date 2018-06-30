5cb2ee50e25de7dd3b380319ccabcdef37d84ea1
sabuj <md.sabuj.sarker@gmail.com>
2018-07-01 02:56:46 +0600

    Refactored directory structure

--- example/index.php
+++ example/index.php
@@ -1,6 +1,8 @@
 <?php
 
+$_halka_file = '../src/halka.php';
+
 define('HALKA_BASEDIR', __DIR__);
 define('HALKA_FRONTSCRIPT', basename(__FILE__));
 
-require_once 'halka.php';
+require_once $_halka_file;
